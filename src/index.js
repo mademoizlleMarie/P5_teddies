db69b2be1acb411fe11e3217730e34f65fb6fb09
sylvestre <sylvestre.marie@gmail.com>
2020-07-19 20:47:19 +0200

    travail sur la page index ajout du css

--- src/index.js
+++ src/index.js
@@ -48,64 +48,119 @@ async function retrieveContent() {
 
 async function showContent() {
     try {
-
         const listeProduit = await retrieveContent();
         console.log(listeProduit);
         let elt = document.getElementById('card');
         for ( let produit of listeProduit) {
-
             var card = document.createElement("div");
-            card.setAttribute("class","card");
+            card.setAttribute("class", "card");
 
             var img = document.createElement("img");
-            img.setAttribute("class","card-img-top");
-            img.setAttribute("src",produit.imageUrl);
+            img.setAttribute("class", "card-img-top");
+            img.setAttribute("src", produit.imageUrl);
 
             var cardBody = document.createElement("div");
-            cardBody.setAttribute("class","card-body");
+            cardBody.setAttribute("class", "card-body");
 
             var title = document.createElement("h5");
-            title.setAttribute("class","card-title");
-            title.innerHTML = produit.name;
+            title.setAttribute("class", "card-title");
+            title.innerHTML = "Adoptez "+ produit.name;
             cardBody.append(title);
 
             var textDescription = document.createElement("p");
-            textDescription.setAttribute("class","card-text");
+            textDescription.setAttribute("class", "card-text");
             textDescription.innerHTML = produit.description;
             cardBody.append(textDescription);
 
-            var textPrix= document.createElement("p");
-            textPrix.setAttribute("class","card-text");
-            textPrix.innerHTML = produit.price/100;
+            var textPrix = document.createElement("p");
+            textPrix.setAttribute("class", "card-text prix");
+            textPrix.innerHTML = " Prix tout doux de : "+produit.price / 100 + " €";
             cardBody.append(textPrix);
 
             var lienProduit = document.createElement("a");
-            lienProduit.setAttribute("class","btn btn-primary");
-            lienProduit.setAttribute("href","produit.html?id=" + produit._id);
-            lienProduit.innerHTML = "voir le produit";
+            lienProduit.setAttribute("class", "btn btn-outline-success");
+            lienProduit.setAttribute("href", "produit.html?id=" + produit._id);
+            lienProduit.innerHTML = "Plus de détail sur " + produit.name;
             cardBody.append(lienProduit);
 
             card.append(img);
             card.append(cardBody);
             elt.append(card);
 
-          /*  elt.innerHTML +=
-                " <div  class=\"card\" style=\"width: 18rem;\">\n"+
-                "  <img class=\"card-img-top\" src="+ produit[id].imageUrl +">\n" +
-                "  <div class=\"card-body\">\n" +
-                "    <h5 class=\"card-title\">" + produit[id].name + "</h5>\n" +
-                "    <p class=\"card-text\">" + produit[id].description + " </p>\n" +
-                "    <p class=\"card-text\">" + produit[id].price/100 + " €</p>\n" +
-                "    <a href=\"#\" id='produit' class=\"btn btn-primary\">voir le produit</a>\n" +
-                "  </div>\n"+
-                "  </div>";*/
         }
-       /* document.getElementsByTagName('body')[0].appendChild(elt);*/
+    } catch (e) {
+        console.log('Error', e);
+    }
+}
+
+async function Carousel() {
+    try {
+        const listeProduit = await retrieveContent();
+        let elt = document.getElementById('carousel');
+
+        var inner = document.createElement("div");
+        inner.setAttribute("class", "carousel-inner");
+
+        var active = document.createElement("div");
+        active.setAttribute("class", "carousel-item active");
+
+        var imgActive = document.createElement("img");
+        imgActive.setAttribute("class", "d-block ");
+        imgActive.setAttribute("src", listeProduit[0].imageUrl);
+
+        imgActive.setAttribute("alt", "First slide");
+        active.append(imgActive);
+
+        var item1 = document.createElement("div");
+        item1.setAttribute("class", "carousel-item");
+
+        var imgItem1 = document.createElement("img");
+        imgItem1.setAttribute("class", "d-block ");
+        imgItem1.setAttribute("src", listeProduit[1].imageUrl);
+        imgItem1.setAttribute("alt", "First slide");
+        item1.append(imgItem1);
+
+        var item2 = document.createElement("div");
+        item2.setAttribute("class", "carousel-item");
+
+        var imgItem2 = document.createElement("img");
+        imgItem2.setAttribute("class", "d-block ");
+        imgItem2.setAttribute("src", listeProduit[2].imageUrl);
+        imgItem2.setAttribute("alt", "First slide");
+        item2.append(imgItem2);
+
+        var item3 = document.createElement("div");
+        item3.setAttribute("class", "carousel-item");
+
+        var imgItem3 = document.createElement("img");
+        imgItem3.setAttribute("class", "d-block ");
+        imgItem3.setAttribute("src", listeProduit[3].imageUrl);
+        imgItem3.setAttribute("alt", "First slide");
+        item3.append(imgItem3);
+
+        var item4 = document.createElement("div");
+        item4.setAttribute("class", "carousel-item");
+
+        var imgItem4 = document.createElement("img");
+        imgItem4.setAttribute("class", "d-block ");
+        imgItem4.setAttribute("src", listeProduit[4].imageUrl);
+        imgItem4.setAttribute("alt", "First slide");
+        item4.append(imgItem4);
+
+        inner.append(active);
+        inner.append(item1);
+        inner.append(item2);
+        inner.append(item3);
+        inner.append(item4);
+        elt.append(inner);
+
+
     } catch (e) {
         console.log('Error', e);
     }
 }
 
 showContent();
+Carousel();
 
 
--- src/produit.js
+++ src/produit.js
@@ -1,3 +1,59 @@
 var url = new URL(window.location);
 var id = url.searchParams.get("id");
-console.log(id);
+
+async function getproduit()
+{
+    const url2 = "http://localhost:3000/api/teddies"
+    const response = await fetch(url2+"/"+id);
+    return response.json();
+}
+
+getproduit()
+
+async function showProduit() {
+    try {
+        const produit = await getproduit();
+        console.log(produit);
+        let elt = document.getElementById('produit');
+
+        var card = document.createElement("div");
+        card.setAttribute("class", "card");
+
+        var img = document.createElement("img");
+        img.setAttribute("class", "card-img-top");
+        img.setAttribute("src", produit.imageUrl);
+
+        var cardBody = document.createElement("div");
+        cardBody.setAttribute("class", "card-body");
+
+        var title = document.createElement("h5");
+        title.setAttribute("class", "card-title");
+        title.innerHTML = produit.name;
+        cardBody.append(title);
+
+        var textDescription = document.createElement("p");
+        textDescription.setAttribute("class", "card-text");
+        textDescription.innerHTML = produit.description;
+        cardBody.append(textDescription);
+
+        var textPrix = document.createElement("p");
+        textPrix.setAttribute("class", "card-text");
+        textPrix.innerHTML = produit.price / 100 + " €";
+        cardBody.append(textPrix);
+
+        var lienProduit = document.createElement("a");
+        lienProduit.setAttribute("class", "btn btn-primary");
+        lienProduit.setAttribute("href", "produit.html?id=" + produit._id);
+        lienProduit.innerHTML = "voir le produit";
+        cardBody.append(lienProduit);
+
+        card.append(img);
+        card.append(cardBody);
+        elt.append(card);
+
+    } catch (e) {
+        console.log('Error', e);
+    }
+}
+
+showProduit();
